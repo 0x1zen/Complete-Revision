1ef8a0a73a369e9a41f151f933445a910d0e52e5
0x1zen <rajdubalwork@gmail.com>
2024-04-09 13:25:12 +0530

    Closures

---
+++ JS-Revision/Fundamentals/closures.js
@@ -0,0 +1,15 @@
+function a(){
+    let b=10;
+    function x(){
+        console.log(b); //  here b is just a identifier or a reference.This doesnt mean that b=10;
+    }
+     b=100;
+    return x; // return complete clouse i.e function + its lexical environment bundled together
+}
+
+let z=a();
+z(); // so if we call z here which contains the function x ,it could print 7, meaning not just the function was returned but the 
+// complete closure was returned
+// 100 will be printed
+
+
