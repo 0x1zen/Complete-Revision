211fc991ae32d2e2535dea28706fb52f7e88e163
0x1zen <rajdubalwork@gmail.com>
2024-04-09 11:18:50 +0530

    errors.js

---
+++ JS-Revision/Fundamentals/errors.js
@@ -0,0 +1,18 @@
+// reference error
+
+console.log(a); // a is not defined
+
+function printValue(){
+    let a=10;
+}
+
+// syntax error
+
+let a=10;
+let a=100;
+
+// type error
+
+const a=10;
+
+a=100;
