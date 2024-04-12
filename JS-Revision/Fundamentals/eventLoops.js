b910b70aaef57fa89ee4f29128f93f5d3aaf4a12
0x1zen <rajdubalwork@gmail.com>
2024-04-12 16:05:43 +0530

    .

--- JS-Revision/Fundamentals/eventLoops.js
+++ JS-Revision/Fundamentals/eventLoops.js
@@ -5,9 +5,13 @@ setTimeout(function cb(){
 
 console.log("End");
 
-// 1.Callback registers in Api
-// 2.After 5 seconds goes into callback queue
-// 3.Event loop after checking call stack,pushes callback function from callback queue to call stack
+// fetch(): fetch("https://google.com") initiates a network request and returns a promise.
+// Promise: The returned promise represents the result of the network request. It will resolve with a response object when the request is
+//  successful, or reject with an error if there is a failure.
+// Attaching Callback: You attach a callback function (cbF) to the promise using the .then() method. This function will be executed when 
+// the promise resolves.
+// Callback Execution: Once the network request is complete and the promise resolves, the callback function (cbF) is enqueued in the 
+// microtask queue and executed by the event loop when the JavaScript call stack is empty.
 
 // Fetch Request
 
