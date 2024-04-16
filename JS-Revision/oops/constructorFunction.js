07d2287b1deba88535ac5690b3faea7a2da4bc6b
0x1zen <rajdubalwork@gmail.com>
2024-04-16 23:36:13 +0530

    Protoype in js

--- JS-Revision/oops/constructorFunction.js
+++ JS-Revision/oops/constructorFunction.js
@@ -5,48 +5,52 @@ function BankAccount(customerName,balance=0){
     this.accountNumber=Date.now();
     this.balance=balance;
 
-    this.deposit=(amount)=>{
-        this.balance+=amount;
-    }
+    // this.deposit=(amount)=>{
+    //     this.balance+=amount;
+    // }
 
     this.withDraw=(amount)=>{
         this.balance-=amount;
     }
 }
 
-// const rajAccount=new BankAccount("Raj",1000);
+BankAccount.prototype.deposit=function (amount){
+    this.balance+=amount;
+}
+
+const rajAccount=new BankAccount("Raj",1000);
 
 // const chetanAccount=new BankAccount("Chetan",15000);
 
 // rajAccount.customerName="Dubal";
 
-// rajAccount.deposit(500);
+rajAccount.deposit(500);
 
 // chetanAccount.withDraw(5000);
 
-// console.log(rajAccount);
+console.log(rajAccount);
 // console.log(chetanAccount);
 
-const accounts=[];
+// const accounts=[];
 
-const accountForm=document.getElementById("accountForm");
-const customerName=document.getElementById("customerName");
-const balance=document.getElementById("balance");
+// const accountForm=document.getElementById("accountForm");
+// const customerName=document.getElementById("customerName");
+// const balance=document.getElementById("balance");
 
-const depositForm=document.getElementById("depositForm");
-const accountNumber=document.getElementById("accountNumber");
-const depositAmount=document.getElementById("depositAmount"); 
+// const depositForm=document.getElementById("depositForm");
+// const accountNumber=document.getElementById("accountNumber");
+// const depositAmount=document.getElementById("depositAmount"); 
 
-accountForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const account=new BankAccount(customerName.value,+balance.value);
-    accounts.push(account);
-    console.log(accounts);
-})
+// accountForm.addEventListener('submit',(e)=>{
+//     e.preventDefault();
+//     const account=new BankAccount(customerName.value,+balance.value);
+//     accounts.push(account);
+//     console.log(accounts);
+// })
 
-depositForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const account=accounts.find((account)=>account.accountNumber===+accountNumber.value);
-    account.deposit(+depositAmount.value);
-    console.log(accounts);
-})
+// depositForm.addEventListener('submit',(e)=>{
+//     e.preventDefault();
+//     const account=accounts.find((account)=>account.accountNumber===+accountNumber.value);
+//     account.deposit(+depositAmount.value);
+//     console.log(accounts);
+// })
