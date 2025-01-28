ed1cc0952985a0af05124438dc516d481ec4881c
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2025-01-28 05:35:31 +0000

    JSX,React Components,Babel

--- React-basics/App.js
+++ React-basics/App.js
@@ -7,7 +7,22 @@ import ReactDOM from "react-dom/client";
 // root.render(heading);
 
 // using JSX
-const jsxHeading = <h1>Hello world from JSX</h1>;
+const heading = <h1 id="hello">Hello world from JSX</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
-console.log(jsxHeading);
+// root.render(heading);
+// console.log(heading);
+function retSom({ num }) {
+  return num == 95 ? "correct" : "not";
+}
+
+const Title = () => <h1 id="title">Welcome To React!</h1>;
+const num = 95;
+const HeadingComponent = ({ num }) => (
+  <div id="heading">
+    {/* <Title /> */}
+    {Title()}
+    {retSom({ num })}
+    <p>This is Interesting!</p>
+  </div>
+);
+root.render(<HeadingComponent num={num} />);
